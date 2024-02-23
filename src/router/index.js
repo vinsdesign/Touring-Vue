e814b3f02645c93c888360335e46d83282b4e333
evinsdesign <kelvinsaputra024@gmail.com>
2024-02-23 13:59:59 +0800

    L1-Building Pagination & update db.json

--- src/router/index.js
+++ src/router/index.js
@@ -3,26 +3,28 @@ import EventListView from '../views/EventListView.vue'
 import AboutView from '../views/AboutView.vue'
 import EventDetailView from '../views/EventDetailView.vue'
 
+const routes = [
+  {
+    path: '/',
+    name: 'event-list',
+    component: EventListView,
+    props: (route) => ({ page: parseInt(route.query.page) || 1 })
+  },
+  {
+    path: '/event/:id',
+    name: 'event-details',
+    props: true,
+    component: EventDetailView
+  },
+  {
+    path: '/about',
+    name: 'about',
+    component: AboutView
+  }
+]
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/',
-      name: 'event-list',
-      component: EventListView
-    },
-    {
-      path: '/event/:id',
-      name: 'event-details',
-      props: true,
-      component: EventDetailView
-    },
-    {
-      path: '/about',
-      name: 'about',
-      component: AboutView
-    }
-  ]
+  routes
 })
 
 export default router
--- src/views/EventListView.vue
+++ src/views/EventListView.vue
@@ -1,21 +1,3 @@
-<script setup>
-import { ref, onMounted } from 'vue'
-import EventCard from '@/components/EventCard.vue'
-import EventService from '@/services/EventService.js'
-
-const events = ref(null)
-
-onMounted(() => {
-  EventService.getEvents()
-    .then((response) => {
-      events.value = response.data
-    })
-    .catch((error) => {
-      console.log(error)
-    })
-})
-</script>
-
 <template>
   <h1>Events For Good</h1>
   <div class="events">
@@ -23,10 +5,67 @@ onMounted(() => {
   </div>
 </template>
 
+<script>
+import EventCard from '@/components/EventCard.vue'
+import EventService from '@/services/EventService'
+import { watchEffect } from 'vue'
+
+export default {
+  name: 'event-list',
+  props: ['page'],
+  components: {
+    EventCard
+  },
+  data() {
+    return {
+      events: null,
+      totalEvents: 0
+    }
+  },
+  created() {
+    watchEffect(() => {
+      this.events = null
+      EventService.getEvents(2, this.page)
+        .then((respone) => {
+          this.events = respone.data
+          this.totalEvents = respone.headers['x-total-count']
+        })
+        .catch((error) => {
+          console.log(error)
+        })
+    })
+  },
+  computed: {
+    hasNextPage() {
+      var totalPages = Math.ceil(this.totalEvents / 2)
+
+      return this.page < totalPages
+    }
+  }
+}
+</script>
+
 <style scoped>
 .events {
   display: flex;
   flex-direction: column;
   align-items: center;
+}
+.pagination {
+  display: flex;
+  width: 290px;
+}
+.pagination a {
+  flex: 1;
+  text-decoration: none;
+  color: #2c3e50;
+}
+
+#page-prev {
+  text-align: left;
+}
+
+#page-next {
+  text-align: right;
 }
 </style>
