0575126335b38a2cedb20733b3b99170707c5a04
evinsdesign <kelvinsaputra024@gmail.com>
2024-02-23 15:09:40 +0800

    fix: myjsonserver

--- src/services/EventService.js
+++ src/services/EventService.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const apiClient = axios.create({
-  baseURL: 'https://my-json-server.typicode.com/vinsdesign/Real-World-Vue-3',
+  baseURL: 'https://my-json-server.typicode.com/vinsdesign/Touring-Vue',
   withCredentials: false,
   headers: {
     Accept: 'application/json',
